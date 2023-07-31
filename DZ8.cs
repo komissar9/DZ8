c1050f61f1f613026ceebfebee760bcad0b14d1a
Alena <magii.alena@yandex.ru>
2023-07-31 18:09:03 +0300

    60 задача

--- DZ8.cs
+++ DZ8.cs
@@ -118,80 +118,97 @@
 // Console.WriteLine($"Строка c наименьшей суммой элементов: {minSum}");
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц
-Console.Write("Введите количество строк первой матрицы: ");
-int rowsOne = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество cтолбцов первой матрицы: ");
-int columnsOne = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество строк второй матрицы: ");
-int rowsTwo = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество cтолбцов второй матрицы: ");
-int columnsTwo = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите мин целое число в массиве: ");
-int minRandom = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите макс целое число в массиве: ");
-int maxRandom = Convert.ToInt32(Console.ReadLine());
-int[,] arrayOne = GetMatrix(rowsOne, columnsOne, minRandom, maxRandom);
-int[,] arrayTwo = GetMatrix(rowsTwo, columnsTwo, minRandom, maxRandom);
-int[,] GetMatrix(int m, int n, int min, int max)
-{
-    int[,] matrix = new int[m, n];
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(min, max);
-        }
-    }
-    return matrix;
-}
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write(matrix[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
+// Console.Write("Введите количество строк первой матрицы: ");
+// int rowsOne = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество cтолбцов первой матрицы: ");
+// int columnsOne = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество строк второй матрицы: ");
+// int rowsTwo = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество cтолбцов второй матрицы: ");
+// int columnsTwo = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите мин целое число в массиве: ");
+// int minRandom = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите макс целое число в массиве: ");
+// int maxRandom = Convert.ToInt32(Console.ReadLine());
+// int[,] arrayOne = GetMatrix(rowsOne, columnsOne, minRandom, maxRandom);
+// int[,] arrayTwo = GetMatrix(rowsTwo, columnsTwo, minRandom, maxRandom);
+// int[,] GetMatrix(int m, int n, int min, int max)
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(min, max);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
 
-}
-Console.WriteLine("Первая матрица:");
-PrintMatrix(arrayOne);
-Console.WriteLine("Вторая матрица:");
-PrintMatrix(arrayTwo);
-// Две матрицы можно перемножить если количество столбцов первой матрицы равно количеству строк второй матрицы.
-int [,] arrayThree = new int [rowsOne, columnsTwo];
-if (columnsOne != rowsTwo)
-{
-    Console.WriteLine("Перемножить матрицы нельзя");
-}
-else
-{
-    for (int i = 0; i < rowsOne; i++)
-    {
-        for (int j = 0; j < columnsTwo; j++)
-        {
-            for (int k = 0; k < columnsOne; k++)
-            {
-                arrayThree [i,j] += arrayOne [i,k] * arrayTwo [k,j];
-            }
-        }
-    }
-    Console.WriteLine("Результат умножения: ");
-    for (int i = 0; i < rowsOne; i++)
-    {
-        for (int j = 0; j < columnsTwo; j++)
-        {
-            Console.Write (arrayThree [i,j] + "\t");
-        }
-        Console.WriteLine();
-    }
+// }
+// Console.WriteLine("Первая матрица:");
+// PrintMatrix(arrayOne);
+// Console.WriteLine("Вторая матрица:");
+// PrintMatrix(arrayTwo);
+// // Две матрицы можно перемножить если количество столбцов первой матрицы равно количеству строк второй матрицы.
+// int [,] arrayThree = new int [rowsOne, columnsTwo];
+// if (columnsOne != rowsTwo)
+// {
+//     Console.WriteLine("Перемножить матрицы нельзя");
+// }
+// else
+// {
+//     for (int i = 0; i < rowsOne; i++)
+//     {
+//         for (int j = 0; j < columnsTwo; j++)
+//         {
+//             for (int k = 0; k < columnsOne; k++)
+//             {
+//                 arrayThree [i,j] += arrayOne [i,k] * arrayTwo [k,j];
+//             }
+//         }
+//     }
+//     Console.WriteLine("Результат умножения: ");
+//     for (int i = 0; i < rowsOne; i++)
+//     {
+//         for (int j = 0; j < columnsTwo; j++)
+//         {
+//             Console.Write (arrayThree [i,j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
 
-}
-
-
+// }
 
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
+Random number = new Random();
+
+  int M = 2, N = 2, K = 2;
+
+  int[,,] array3D = new int[M, N, K];
+
+  for (int i = 0; i < M; i++)
+  {
+    for (int j = 0; j < N; j++)
+    {
+      for (int l = 0; l < K; l++)
+      {
+        array3D[i, j, l] = number.Next(10, 99);
+        Console.Write($"{array3D[i, j, l]}({i},{j},{l}) ");
+      }
+      Console.WriteLine();
+    }
+  } 
+
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
